6017f6b48d2bd13cfd56cab36c04aa35f4b1713e
facundenks <facundenks20@gmail.com>
2019-10-02 01:25:38 -0300

    Este es el proyecto

---
+++ Tp1_lpoo2_2019/Vistas/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Usuario> usuarios = new List<Usuario>();
+        private String rol;
+
+        public String Rol
+        {
+            get { return rol; }
+            set { rol = value; }
+        }
+        private String nombreApellido;
+
+        public String NombreApellido
+        {
+            get { return nombreApellido; }
+            set { nombreApellido = value; }
+        }
+
+        public List<Usuario> Usuarios
+        {
+            get { return usuarios; }
+            set { usuarios = value; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            validar();
+        }
+
+        private void validar()
+        {
+            if (Rol == "administrador")
+            {
+                vent.Visibility = Visibility.Visible;
+                ges.Visibility = Visibility.Hidden;
+            }
+            else {
+                if (Rol == "operador")
+                {
+                    vent.Visibility = Visibility.Hidden;
+                    ges.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        private void MenuItem_Cliente(object sender, RoutedEventArgs e)
+        {
+            formCliente cleinteForm = new formCliente();
+            cleinteForm.Show();
+        }
+
+        private void MenuItem_Autobus(object sender, RoutedEventArgs e)
+        {
+            formAutobus autobusForm = new formAutobus();
+            autobusForm.Show();
+        }
+
+        private void MenuItem_Pasaje(object sender, RoutedEventArgs e)
+        {
+            formVenta ventaForm = new formVenta();
+            ventaForm.Show();
+        }
+    }
+}
---
+++ Tp1_lpoo2_2019/Vistas/formAutobus.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para formAutobus.xaml
+    /// </summary>
+    public partial class formAutobus : Window
+    {
+        Autobus oAutobus = new Autobus();
+
+        public formAutobus()
+        {
+            InitializeComponent();
+        }
+
+        private void txtId_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void txtCapacidad_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                e.Handled = false;
+            else
+                e.Handled = true;
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtId.Text != "" && txtCapacidad.Text != "" && cmbServicio.SelectedIndex != -1 && txtPatente.Text != "")
+            {
+                oAutobus.Aut_codigo = Convert.ToInt32(txtId.Text);
+                oAutobus.Aut_capacidad = Convert.ToInt32(txtCapacidad.Text);
+                oAutobus.Aut_tipoServicio = Convert.ToString(((ComboBoxItem)cmbServicio.SelectedItem).Content);
+                oAutobus.Aut_matricula = txtPatente.Text;
+
+                limpiar();
+
+                MessageBox.Show("Codigo: "+oAutobus.Aut_codigo+"\n Capacidad: "+oAutobus.Aut_capacidad+
+                    "\n Servicio: "+oAutobus.Aut_tipoServicio+"\n Matricula: "+oAutobus.Aut_matricula);
+            }
+            else
+            {
+                MessageBox.Show("Completar todos los campos");
+            }
+        }
+
+        private void limpiar() {
+            txtId.Clear();
+            txtCapacidad.Clear();
+            cmbServicio.SelectedIndex = -1;
+            txtPatente.Clear();
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            limpiar();
+        }
+    }
+}
